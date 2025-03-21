447a2fb973480a8f3f7faf9f12634aea8c56327a
AgusNolasco <nolasco@dc.exa.unrc.edu.ar>
2025-03-21 15:30:30 -0300

    Now we iterate over different optimization levels

--- run_experiments.py
+++ run_experiments.py
@@ -6,23 +6,26 @@ Ns = []
 for m in range(1,M+1):
     Ns.append(4*m**3)
 
-for N in Ns:
-    print('Running: make clean')
-    clean_result = subprocess.run(["make", "clean"], capture_output=True, text=True)
+opt_flags = ['-O0', '-O1', '-O2', '-O3'] # Podriamos agregar fast math
 
-    if clean_result.returncode != 0:
-        raise Exception("Make clean failed.")
+for opt in opt_flags:
+    for N in Ns:
+        print('Running: make clean')
+        clean_result = subprocess.run(["make", "clean"], capture_output=True, text=True)
 
-    print(f'Running: make "CPPFLAGS="-DN={N}" ')
-    make_result = subprocess.run(["make", f'CPPFLAGS="-DN={N}"'], capture_output=True, text=True)
+        if clean_result.returncode != 0:
+            raise Exception("Make clean failed.")
 
-    if make_result.returncode != 0:
-        raise Exception("Make failed.")
+        print(f'Running: make CPPFLAGS="-DN={N}" CFLAGS="{opt}"')
+        make_result = subprocess.run(["make", f'CPPFLAGS="-DN={N}"', f'CFLAGS="{opt}"'], capture_output=True, text=True)
 
-    for _ in range(0,5):
-        result = subprocess.run(["./tiny_md"], capture_output=True, text=True)
-        output = result.stdout
-        for line in output.split("\n"):
-            if '***' in line:
-                print(line)
+        if make_result.returncode != 0:
+            raise Exception("Make failed.")
+
+        for _ in range(0,5):
+            result = subprocess.run(["./tiny_md"], capture_output=True, text=True)
+            output = result.stdout
+            for line in output.split("\n"):
+                if '***' in line:
+                    print(line)
 
