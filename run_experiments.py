25547e163ab42a3825ca96687cf69084b8789125
CP2025 <cp2025_101@debian-BULLSEYE-live-builder-AMD64>
2025-03-22 16:46:16 -0300

    make script to save a csv file with particulas/s, N, opt and compiler headers, add running experiments with clang
    Signed-off-by: CP2025 <cp2025_101@debian-BULLSEYE-live-builder-AMD64>

--- run_experiments.py
+++ run_experiments.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 import subprocess
+import csv
+import datetime
+import os
+
+os.makedirs('results', exist_ok=True)
+timestamp = datetime.datetime.now().strftime('%Y%m%dT%H%M%S')
 
 M = 4
 Ns = []
@@ -7,25 +13,40 @@ for m in range(1,M+1):
     Ns.append(4*m**3)
 
 opt_flags = ['-O0', '-O1', '-O2', '-O3'] # Podriamos agregar fast math
+compilers = ['gcc', 'clang']
+results = []
 
-for opt in opt_flags:
-    for N in Ns:
-        print('Running: make clean')
-        clean_result = subprocess.run(["make", "clean"], capture_output=True, text=True)
+for compiler in compilers:
+    for opt in opt_flags:
+        for N in Ns:
+            print('Running: make clean')
+            clean_result = subprocess.run(["make", "clean"], capture_output=True, text=True)
 
-        if clean_result.returncode != 0:
-            raise Exception("Make clean failed.")
+            if clean_result.returncode != 0:
+                raise Exception("Make clean failed.")
 
-        print(f'Running: make CPPFLAGS="-DN={N}" CFLAGS="{opt}"')
-        make_result = subprocess.run(["make", f'CPPFLAGS="-DN={N}"', f'CFLAGS="{opt}"'], capture_output=True, text=True)
+            print(f'Running: make CC="{compiler}" CPPFLAGS="-DN={N}" CFLAGS="{opt}"')
+            make_result = subprocess.run(["make", f'CC="{compiler}"', f'CPPFLAGS="-DN={N}"', f'CFLAGS="{opt}"'], capture_output=True, text=True)
 
-        if make_result.returncode != 0:
-            raise Exception("Make failed.")
+            if make_result.returncode != 0:
+                raise Exception("Make failed.")
 
-        for _ in range(0,5):
-            result = subprocess.run(["./tiny_md"], capture_output=True, text=True)
-            output = result.stdout
-            for line in output.split("\n"):
-                if '***' in line:
-                    print(line)
+            for _ in range(0,5):
+                result = subprocess.run(["./tiny_md"], capture_output=True, text=True)
+                output = result.stdout
+                for line in output.split("\n"):
+                    if '***' in line:
+                        particulas_s = f'{
+                            float(
+                                line.split('***')[1] # sacar asteriscos
+                                .replace('particulas/s:', "") # sacar el string "particulas/s:"
+                                .strip() # remover cualquier leading/trailing whitespace
+                                ):.3f
+                            }'
+                        results.append((particulas_s, N, opt, compiler))
+                        print(line)
 
+with open(f'results/{timestamp}.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(['particulas/s', 'N', 'opt', 'compiler'])
+    writer.writerows(results)
