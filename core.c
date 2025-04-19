fd981ece6b3863d9efd02e4924a8f9e0b9c1c7fc
AgusNolasco <nolasco@dc.exa.unrc.edu.ar>
2025-04-19 17:48:47 -0300

    Manually vectorize the quadratic loop in forces using intrinsics. Also add a vectorization-friendly version of minimum image function using masking.

--- core.c
+++ core.c
@@ -3,6 +3,7 @@
 
 #include <math.h>
 #include <stdlib.h> // rand()
+#include <immintrin.h>
 
 #define ECUT (4.0 * (pow(RCUT, -12) - pow(RCUT, -6)))
 
@@ -22,7 +23,7 @@ void init_pos(double* rxyz, const double rho)
                 rxyz[idx + 1] = j * a; // y
                 rxyz[idx + 2] = k * a; // z
                 rxyz[idx + 3] = 0; // z
-                    // del mismo átomo
+                                   // del mismo átomo
                 rxyz[idx + 4] = (i + 0.5) * a;
                 rxyz[idx + 5] = (j + 0.5) * a;
                 rxyz[idx + 6] = k * a;
@@ -79,7 +80,7 @@ void init_vel(double* vxyz, double* temp, double* ekin)
     sf = sqrt(T0 / *temp);
 
     for (int i = 0; i < 4 * N; i += 4) { // elimina la velocidad del centro de masa
-        // y ajusta la temperatura
+                                         // y ajusta la temperatura
         vxyz[i + 0] = (vxyz[i + 0] - sumvx) * sf;
         vxyz[i + 1] = (vxyz[i + 1] - sumvy) * sf;
         vxyz[i + 2] = (vxyz[i + 2] - sumvz) * sf;
@@ -100,9 +101,29 @@ static inline double minimum_image(double cordi, const double cell_length)
     return cordi;
 }
 
+static inline __m256d minimum_image_avx(__m256d cords, double cell_length) {
+    __m256d half_cell_length = _mm256_set1_pd(0.5 * cell_length);
+    __m256d full_cell_length = _mm256_set1_pd(cell_length);
 
-void forces(const double* rxyz, double* fxyz, double* epot, double* pres,
-            const double* temp, const double rho, const double V, const double L)
+    // mask_lt: cords <= -0.5 * cell_length
+    __m256d minus_half_cell_length = _mm256_sub_pd(_mm256_setzero_pd(), half_cell_length);
+    __m256d mask_lt = _mm256_cmp_pd(cords, minus_half_cell_length, _CMP_LE_OQ);
+
+    // mask_gt: cords > 0.5 * cell_length
+    __m256d mask_gt = _mm256_cmp_pd(cords, half_cell_length, _CMP_GT_OQ);
+
+    // Apply cocordscordsections
+    __m256d add_cell_length = _mm256_add_pd(cords, full_cell_length);
+    __m256d sub_cell_length = _mm256_sub_pd(cords, full_cell_length);
+
+    // Blend cordsesults
+    __m256d cords_tmp = _mm256_blendv_pd(cords, add_cell_length, mask_lt);
+    return _mm256_blendv_pd(cords_tmp, sub_cell_length, mask_gt);
+}
+
+
+void forces(const double* restrict rxyz, double* restrict fxyz, double* restrict epot, double* restrict pres,
+        const double* restrict temp, const double rho, const double V, const double L)
 {
     // calcula las fuerzas LJ (12-6)
 
@@ -115,50 +136,56 @@ void forces(const double* rxyz, double* fxyz, double* epot, double* pres,
 
     for (int i = 0; i < 4 * (N - 1); i += 4) {
 
-        double xi = rxyz[i + 0];
-        double yi = rxyz[i + 1];
-        double zi = rxyz[i + 2];
-        double wi = rxyz[i + 3];
+        __m256d ri = _mm256_loadu_pd(rxyz + i);
 
         for (int j = i + 4; j < 4 * N; j += 4) {
 
-            double xj = rxyz[j + 0];
-            double yj = rxyz[j + 1];
-            double zj = rxyz[j + 2];
-            double wj = rxyz[j + 3];
+            __m256d rj = _mm256_loadu_pd(rxyz + j);
 
-            // distancia mínima entre r_i y r_j
-            double rx = xi - xj;
-            double ry = yi - yj;
-            double rz = zi - zj;
-            double rw = wi - wj;
-            rx = minimum_image(rx, L);
-            ry = minimum_image(ry, L);
-            rz = minimum_image(rz, L);
+            // rij = ri - rj
+            __m256d rij = _mm256_sub_pd(ri, rj);
 
-            double rij2 = rx * rx + ry * ry + rz * rz + rw * rw;
+            rij = minimum_image_avx(rij, L);
 
-            if (rij2 <= rcut2) {
-                double r2inv = 1.0 / rij2;
+            // rij2 = rij * rij
+            __m256d rij2 = _mm256_mul_pd(rij, rij); // [da|db|dc|dd]
+
+            __m256d shuf1 = _mm256_permute2f128_pd(rij2, rij2, 1); // [dc|dd|da|db]
+            __m256d sum1 = _mm256_add_pd(rij2, shuf1); // [da+dc|db+dd|dc+da|dd+db]
+            __m128d lo128 = _mm256_castpd256_pd128(sum1); // [da+dc|db+dd]
+            __m128d hi128 = _mm_unpackhi_pd(lo128, lo128); // [db+dd|db+dd]
+            __m128d total = _mm_add_sd(lo128, hi128); // [da+dc+db+dd|db+dd+db+dd]
+            double rij2_scalar;
+            _mm_store_sd(&rij2_scalar, total); // [da+dc+db+dd] 
+            
+            // Esto vale mas la pena con precision-simple
+            //__m256d sumpd = _mm256_hadd_pd(rij2,rij2);
+            //double rij2_scalar = _mm_cvtsd_f64(_mm_add_pd(_mm256_extractf128_pd(sumpd,0), _mm256_extractf128_pd(sumpd,1)));
+
+            // Mask if rij2 <= rcut2
+            if (rij2_scalar <= rcut2) {
+                double r2inv = 1.0 / rij2_scalar;
                 double r6inv = r2inv * r2inv * r2inv;
 
                 double fr = 24.0 * r2inv * r6inv * (2.0 * r6inv - 1.0);
 
-                fxyz[i + 0] += fr * rx;
-                fxyz[i + 1] += fr * ry;
-                fxyz[i + 2] += fr * rz;
-                fxyz[i + 3] += fr * rw;
+                __m256d frc = _mm256_mul_pd(_mm256_set1_pd(fr), rij);
 
-                fxyz[j + 0] -= fr * rx;
-                fxyz[j + 1] -= fr * ry;
-                fxyz[j + 2] -= fr * rz;
-                fxyz[j + 3] -= fr * rw;
+                __m256d fi = _mm256_loadu_pd(fxyz + i);
+                __m256d fj = _mm256_loadu_pd(fxyz + j);
+
+                fi = _mm256_add_pd(fi, frc);
+                fj = _mm256_sub_pd(fj, frc);
+
+                _mm256_storeu_pd(fxyz + i, fi);
+                _mm256_storeu_pd(fxyz + j, fj);
 
                 *epot += 4.0 * r6inv * (r6inv - 1.0) - ECUT;
-                pres_vir += fr * rij2;
+                pres_vir += fr * rij2_scalar;
             }
         }
     }
+
     pres_vir /= (V * 3.0);
     *pres = *temp * rho + pres_vir;
 }
@@ -177,8 +204,8 @@ static inline double pbc(double cordi, const double cell_length)
 
 
 void velocity_verlet(double* restrict rxyz, double* restrict vxyz, double* restrict fxyz, double* epot,
-                     double* ekin, double* pres, double* temp, const double rho,
-                     const double V, const double L)
+        double* ekin, double* pres, double* temp, const double rho,
+        const double V, const double L)
 {
 
     for (int i = 0; i < 4 * N; i += 4) { // actualizo posiciones
@@ -187,6 +214,9 @@ void velocity_verlet(double* restrict rxyz, double* restrict vxyz, double* restr
         rxyz[i + 2] += vxyz[i + 2] * DT + 0.5 * fxyz[i + 2] * DT * DT;
         rxyz[i + 3] += vxyz[i + 3] * DT + 0.5 * fxyz[i + 3] * DT * DT;
 
+        // TODO: Se me ocurre que esta bloque se puede sacar, porque es el unico que no podemos
+        // vectorizar facilmente, debido a la llamada a pbc, que si bien esta inline capaz produce
+        // que se desvien algunas lanes que van en paralelo debido al branching.
         rxyz[i + 0] = pbc(rxyz[i + 0], L);
         rxyz[i + 1] = pbc(rxyz[i + 1], L);
         rxyz[i + 2] = pbc(rxyz[i + 2], L);
