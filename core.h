dda98174a918bd9602eed0264417048c71a0cdee
luciano <lucianozablocki@gmail.com>
2025-04-19 19:23:13 -0300

    add missing file
    Signed-off-by: luciano <lucianozablocki@gmail.com>

--- core.h
+++ core.h
@@ -1,8 +1,10 @@
 #ifndef CORE_H
 #define CORE_H
+#include <stdio.h>
+#include "mtwister.h"
 
 void init_pos(double* rxyz, const double rho);
-void init_vel(double* vxyz, double* temp, double* ekin);
+void init_vel(double* vxyz, double* temp, double* ekin, MTRand* r);
 void forces(const double* rxyz, double* fxyz, double* epot, double* pres,
             const double* temp, const double rho, const double V, const double L);
 void velocity_verlet(double* rxyz, double* vxyz, double* fxyz, double* epot,
